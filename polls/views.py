724d8c2076eb071a66797890de69a64ca48eb30f
danielTiringer <tiringerdaniel@gmail.com>
2020-04-28 08:12:46 +0200

    fix(import): missing http404 import

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 
