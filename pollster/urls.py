e98d596427782e5931a2c9dbcf8dbd80c2487ee3
danielTiringer <tiringerdaniel@gmail.com>
2020-04-28 08:01:20 +0200

    feat(vote): implement vote view and functionality

--- pollster/urls.py
+++ pollster/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
+    path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('favicon.ico'))),
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
 ]
